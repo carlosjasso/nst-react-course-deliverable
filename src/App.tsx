46c4bc5a8828f85e86056561948f5aede6d63ee8
Carlos Jasso <contact@carlosjasso.dev>
2022-07-21 18:04:01 -0500

    started building quiz app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+interface Colors {
+  [name: string]: string
+}
+
+// coolors palette: https://coolors.co/cacf85-8cba80-658e9c-4d5382-514663
+const backgourndColors : Colors = {
+  "Dark Khaki": "#cacf85",
+  "Dark Sea Green": "#8cba80",
+  "Steel Teal": "#658e9c",
+  "Purple Navy": "#4d5382",
+  "English Violet": "#514663"
+}
+
+let backgroundColor: string;
+
+function onStartQuickClick() {
+  console.log("click!");
+}
+
+function getRandomBackgroundColor(): string {
+  const colorIndex = Math.floor(Math.random() * (Object.keys(backgourndColors).length - 1) + 1);
+  const colorKey = Object.keys(backgourndColors)[colorIndex];
+  return backgourndColors[colorKey];
+}
+
 function App() {
+  backgroundColor = getRandomBackgroundColor();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main-container' style={{ backgroundColor: backgroundColor }}>
+      <section className='container'>
+        <button onClick={onStartQuickClick}>Start Quiz</button>
+      </section>
+    </main>
   );
 }
 
